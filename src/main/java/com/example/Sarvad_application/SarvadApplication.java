7cbc1364bfddbe5b6510c176daadd33b5811adb1
arun.rade <arun.rade@gmail.com>
2025-01-12 20:49:27 +0530

    java code

---
+++ src/main/java/com/example/Sarvad_application/SarvadApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sarvad_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarvadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarvadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sarvad_application/SarvadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sarvad_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarvadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
